cf947ff88ce131cf8c6f2f97b378f679830402f8
Amiraliarzande <amiraliarzande8@gmail.com>
2025-05-27 19:23:49 +0330

    Preparing static and media directories
    Preparing static and media directories

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,7 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static/',  # Directory for custom static files
+STATIC_ROOT = BASE_DIR / 'static/'  # Directory for custom static files
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'statics/',  # Additional directory for static files
+]
     
 
 
