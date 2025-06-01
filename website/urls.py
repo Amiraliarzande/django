dc4bc46dba32a38cc5275b374770a003c946a44b
Amiraliarzande <amiraliarzande8@gmail.com>
2025-06-01 20:32:01 +0330

    Dynamic url
    Dynamic url

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from website.views import *
 
+app_name = 'website'
+
 urlpatterns = [
-    path('', home),
-    path('about', about),
-    path('contact', contact),
-    path('elements', elements)
+    path('', home, name='home'),
+    path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
+    path('elements/', elements, name='elements'),
 ]
