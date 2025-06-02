5a620a36849047c835b8aaf903febd3e84d2c5fc
Amiraliarzande <amiraliarzande8@gmail.com>
2025-06-02 17:01:30 +0330

    Making a blog app
    Making a blog app

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',  # Custom app for the website
+    'blog',  # Custom app for the blog
 ]
 
 MIDDLEWARE = [
