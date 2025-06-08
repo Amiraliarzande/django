70c4b0c6001f95662b3f3a568f0d74b30968d5b5
Amiraliarzande <amiraliarzande8@gmail.com>
2025-06-08 16:09:06 +0330

    Creating an admin link and creating a super admin

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
+from django.contrib import admin
 from django.urls import path
 from blog.views import *
 
 app_name = 'blog'
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', blog_home, name='blog_home'),
     path('single/', blog_single, name='blog_single'),
    
--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,11 @@
+from django.contrib import admin
 from django.urls import path
 from website.views import *
 
 app_name = 'website'
 
 urlpatterns = [
+    path('admin', admin.site.urls),
     path('', home, name='home'),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
