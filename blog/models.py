3859601fed5a25718325c47209ed78513aaac1bf
Amiraliarzande <amiraliarzande8@gmail.com>
2025-06-11 22:18:43 +0330

    Blog post modulation completion
    Blog post modulation completion

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class post (models.Model):
     title = models.CharField(max_length=255)
     content= models.TextField()
+
+    counted_viwe= models.IntegerField(default=0)
+    status = models.BooleanField(default=True)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
