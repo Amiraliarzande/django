7d98a5d430355c430f21b11069ef23f672eae693
Amiraliarzande <amiraliarzande8@gmail.com>
2025-06-12 18:39:01 +0330

    Customize the admin panel post blog
    Customize the admin panel post blog

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from blog.models import post
 
 # Register your models here.
+class postAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    list_display = ('title', 'counted_viwe', 'status', 'published_date', 'created_date', 'updated_date')
+    list_filter = ('status',)
+    search_fields = ['title', 'content']
 
-admin.site.register(post)
 
-
+admin.site.register(post, postAdmin)
