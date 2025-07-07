0d7753323c97406acb7ca0cd21247e4fa3f1df06
Amiraliarzande <amiraliarzande8@gmail.com>
2025-07-07 20:08:58 +0330

    I created a test query using the template tag.
    Using the template tag, I created a test query that passed the number of published posts to the single blog page.

--- blog/templatetags/blog_tag.py
+++ blog/templatetags/blog_tag.py
@@ -1,3 +1,10 @@
 from django import template
+from blog.models import post
 
 register = template.Library()
+
+@register.simple_tag
+def status_post ():
+    post_count = post.objects.filter(status=1).count()
+    return post_count
+    
