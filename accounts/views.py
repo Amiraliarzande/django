4c968b8e30fa4a9cc800660513833082ae60f21b
Amiraliarzande <amiraliarzande8@gmail.com>
2025-07-27 17:34:25 +0330

    Add a login section to the site

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
 def login_view(request):
-    # Logic for handling login
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login successful.")
+            return redirect('/')
+        else:
+            messages.error(request, "Invalid username or password.")
+            return redirect('/')
     return render(request, 'account/login.html')
 
 # def logout_view(request):
