90baf9c97e2bb7fd65dc1771215d33ea2f39fff6
Amiraliarzande <amiraliarzande8@gmail.com>
2025-07-28 13:36:10 +0330

    Login using AuthenticationForm

--- accounts/views.py
+++ accounts/views.py
@@ -1,22 +1,35 @@
 from django.shortcuts import render, redirect 
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 
 def login_view(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            messages.success(request, "Login successful.")
-            return redirect('/')
-        else:
-            messages.error(request, "Invalid username or password.")
-            return redirect('/')
-    return render(request, 'account/login.html')
+            
+    if not request.user.is_authenticated:
+    
+        if request.method == "POST":
+            form = AuthenticationForm(request=request, data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    messages.success(request, "Login successful.")
+                    return redirect('/')
+            
+                
+        form = AuthenticationForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'account/login.html', context)
+    
+    else:
+        return redirect('/')
+        
 
 # def logout_view(request):
     # Logic for handling logout
